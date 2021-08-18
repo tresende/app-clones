b89e86ef4993c4d8f373b2e116e59404b34216e9
Thiago Resende <thiago.gcresende@gmail.com>
2021-08-18 20:32:02 -0300

    🐛  fix eslint 2021

---
+++ nubank/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import * as S from './styles'
+
+const Header = () => (
+  <S.Container>
+    <S.Top>
+      <Text>Hello!</Text>
+    </S.Top>
+  </S.Container>
+)
+
+export default Header
---
+++ nubank/src/components/Tabs/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Text, StyleSheet, StatusBar } from 'react-native'
+
+import * as S from './styles'
+
+const Tabs = () => (
+  <S.Container>
+    <Text>Hello!</Text>
+  </S.Container>
+)
+
+export default Tabs
